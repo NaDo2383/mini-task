ee190c0e58441e01a516d2651675a22515d398fb
NaDo2383 <narandorj.byambadorj@gmail.com>
2023-01-27 16:03:43 +0800

    20230127 home

--- src/App.js
+++ src/App.js
@@ -1,58 +1,62 @@
-import { useEffect, useState } from 'react';
-import './App.css';
-import Button from './components/button';
-import Products from './components/products';
-import Search from './components/search';
-import { product, productCat } from "./components/data"
+import { useEffect, useState } from "react";
+import "./App.css";
+import Button from "./components/button";
+import Products from "./components/products";
+import Search from "./components/search";
+import { product, productCat } from "./components/data";
 
 function App() {
-  const [data, setData] = useState(product);
-  const [checked, setChecked] = useState(false);
+    const [data, setData] = useState(product);
+    const [checked, setChecked] = useState(false);
 
-  // useEffect(() => {
-  //   if (checked) {
-  //     let newArr1 = [];
-  //     product.map(e => {
-  //       if (e.isStock) {
-  //         setData(newArr1)
-  //       }
-  //     })
-  //   } else {
-  //     setData(product)
-  //   }
-  // }, [])
-  function OnlyProductsInStock() {
-    setChecked(!checked)
-    if (checked) {
-      let newArr1 = [];
-      product.map(e => {
-        if (e.isStock) {
-          newArr1.push(e)
-          setData(newArr1)
+    // useEffect(() => {
+    //   if (checked) {
+    //     let newArr1 = [];
+    //     product.map(e => {
+    //       if (e.isStock) {
+    //         setData(newArr1)
+    //       }
+    //     })
+    //   } else {
+    //     setData(product)
+    //   }
+    // }, [])
+    function OnlyProductsInStock(para) {
+        setChecked(!checked);
+        if (para) {
+            let newArr1 = [];
+            product.map((e) => {
+                if (e.isStock) {
+                    newArr1.push(e);
+                    setData(newArr1);
+                }
+            });
+        } else {
+            setData(product);
         }
-      })
-    } else {
-      setData(product)
     }
-  }
 
-  function search(para) {
-    let newArr = [];
-    product.map(e => {
-      if (e.name.includes(para)) {
-        newArr.push(e)
-      }
-      setData(newArr)
-    })
-  }
+    function search(para) {
+        let newArr = [];
+        product.map((e) => {
+            if (e.name.includes(para)) {
+                newArr.push(e);
+            }
+            setData(newArr);
+        });
+    }
 
-  return (
-    <div id='container'>
-      <Search search={search} />
-      <Button setChecked={setChecked} checked={checked} OnlyProductsInStock={OnlyProductsInStock} />
-      <Products setData={setData} data={data} productCat={productCat} />
-    </div>
-  );
+    return (
+        <div id="container">
+            <Search search={search} />
+            <Button
+                setChecked={setChecked}
+                checked={checked}
+                OnlyProductsInStock={OnlyProductsInStock}
+            />
+            <Products setData={setData} data={data} productCat={productCat} />
+        </div>
+    );
 }
 
 export default App;
--- src/components/button.js
+++ src/components/button.js
@@ -1,16 +1,19 @@
-import React from 'react'
-import "./button.css"
+import React from "react";
+import "./button.css";
 
 export default function Button({ checked, setChecked, OnlyProductsInStock }) {
     return (
-        <div id='toggleContainer'>
+        <div id="toggleContainer">
             <label class="container" for="button">
-                <input type="checkbox" id="button" onChange={OnlyProductsInStock} />
+                <input
+                    type="checkbox"
+                    id="button"
+                    checked={checked}
+                    onChange={(e) => OnlyProductsInStock(e.target.checked)}
+                />
                 <span class="slider"></span>
             </label>
-            <div>
-                Only products in stock
-            </div>
+            <div>Only products in stock</div>
         </div>
-    )
+    );
 }
