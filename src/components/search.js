aeee84d72acc870711175df9950fa9dc5ee660b8
NaDo2383 <narandorj.byambadorj@gmail.com>
2023-01-28 11:03:59 +0800

    20230128 class

--- src/components/search.js
+++ src/components/search.js
@@ -5,7 +5,7 @@ export default function Search({ search }) {
     return (
         <div>
             <div id='searchBarContainer'>
-                <input type="search" placeholder='Search...' onChange={e => search(e.target.value)} id='searchbar' />
+                <input type="text" placeholder='Search...' onChange={e => search(e.target.value)} id='searchbar' />
                 <i class="bi bi-search"></i>
             </div>
         </div>
